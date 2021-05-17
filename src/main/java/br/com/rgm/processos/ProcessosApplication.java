6a83a5a4d1215c9894894d977800a9fb4b547c9a
mikansc <michael.nsc@outlook.com>
2021-05-17 20:21:56 -0300

    commit inicial

---
+++ src/main/java/br/com/rgm/processos/ProcessosApplication.java
@@ -0,0 +1,13 @@
+package br.com.rgm.processos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rgm/processos/ProcessosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rgm.processos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
