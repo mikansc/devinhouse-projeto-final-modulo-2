e5de16b10dc297d30f253ee24bea61e2377b2f7e
mikansc <michael.nsc@outlook.com>
2021-05-21 16:12:46 -0300

    Implementa exceções ObjectNotFound

--- src/main/java/br/com/rgm/processos/services/InteressadoService.java
+++ src/main/java/br/com/rgm/processos/services/InteressadoService.java
@@ -1,5 +1,6 @@
 package br.com.rgm.processos.services;
 
+import br.com.rgm.processos.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,8 @@ public class InteressadoService {
 	}
 	
 	public Interessado buscarInteressado(Integer id) {
-		return interessadoRepository.findById(id).get();
+		return interessadoRepository.findById(id)
+				.orElseThrow(()-> new ObjectNotFoundException("Nenhum interessado encontrado com o ID informado"));
 	}
 	
 	public Interessado buscarInteressado(String nuIdentificacao) {
