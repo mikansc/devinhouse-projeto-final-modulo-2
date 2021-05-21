5cd6e5d885c56bd7cfe1d7296416dbd2000a1609
mikansc <michael.nsc@outlook.com>
2021-05-21 16:37:01 -0300

    Adiciona validação nos DTO

--- src/main/java/br/com/rgm/processos/dto/InteressadoDTO.java
+++ src/main/java/br/com/rgm/processos/dto/InteressadoDTO.java
@@ -1,28 +1,34 @@
 package br.com.rgm.processos.dto;
 
-import java.util.Date;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
 
 @NoArgsConstructor
 public class InteressadoDTO {
 
-	@Getter
-	@Setter
-	private Integer id;
+    @Getter
+    @Setter
+    private Integer id;
 
-	@Getter
-	@Setter
-	private String nmInteressado;
+    @Getter
+    @Setter
+    @NotBlank(message = "Você precisa informar um nome")
+    private String nmInteressado;
 
-	@Getter
-	@Setter
-	private String nuIdentificacao;
+    @Getter
+    @Setter
+    @NotBlank(message = "Você precisa informar um número de documento")
+    @CPF(message = "Você precisa informar um número de CPF válido")
+    private String nuIdentificacao;
 
-	@Getter
-	@Setter
-	private Date dtNascimento;
+    @Getter
+    @Setter
+    @NotBlank(message = "Você precisa informar uma data de nascimento válida")
+    private Date dtNascimento;
 
 }
